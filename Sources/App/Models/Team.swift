f47b75fcea205b458889dca94b128b559f15858e
Carlos Aguilar <caguilar@dwdandsolutions.com>
2023-08-13 19:25:16 -0400

    Add database information to team template
    Signed-off-by: Carlos Aguilar <caguilar@dwdandsolutions.com>

---
+++ Sources/App/Models/Team.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carlos Aguilar on 8/13/23.
+//
+
+import Fluent
+import Vapor
+
+final class Team: Model, Content {
+    static let schema: String = "teams"
+
+    @ID(custom: "id", generatedBy: .database)
+    var id: Int?
+
+    @Field(key: "category")
+    var category: String
+
+    @Field(key: "first_name")
+    var firstName: String
+
+    @Field(key: "last_name")
+    var lastName: String
+
+    @Field(key: "position")
+    var position: String
+
+    @Field(key: "company")
+    var company: String
+
+    init() {}
+
+    init(id: Int? = nil, category: String, firstName: String, lastName: String, position: String, company: String) {
+        self.id = id
+        self.category = category
+        self.firstName = firstName
+        self.lastName = lastName
+        self.position = position
+        self.company = company
+    }
+}
